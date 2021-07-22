5982571093b1aeee235af7dcb02dd05a39147227
KYJ <devilasu@naver.com>
2021-07-22 17:40:57 +0900

    switch

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -7,16 +7,33 @@ public class Hello {
 		Scanner s = new Scanner(System.in);
 		String str = s.nextLine();
 		s.close();
-		if(str.equals("a") || str.equals("A"))
+		switch(str) {
+		case "a","A":
 			System.out.println("Apple");
-		else if(str.equals("b") || str.equals("B"))
+			break;
+		case "b","B":
 			System.out.println("Banana");
-		else if(str.equals("w") || str.equals("W"))
+			break;
+		case "w","W":
 			System.out.println("Watermelon");
-		else if(str.equals("s") || str.equals("S"))
-			System.out.println("Strawberry");
-		else
+			break;
+		case "s","S":
+			System.out.println("Strwaberry");
+			break;
+		default:
 			System.out.println("모르는 이니셜");
+		}
 		System.out.println("프로그램 종료");
+//		if(str.equals("a") || str.equals("A"))
+//			System.out.println("Apple");
+//		else if(str.equals("b") || str.equals("B"))
+//			System.out.println("Banana");
+//		else if(str.equals("w") || str.equals("W"))
+//			System.out.println("Watermelon");
+//		else if(str.equals("s") || str.equals("S"))
+//			System.out.println("Strawberry");
+//		else
+//			System.out.println("모르는 이니셜");
+//		System.out.println("프로그램 종료");
 	}
 }
