32d8f117b50a2ebf99171c39cfc968198eff69cf
choichiwon <ccu12345@naver.com>
2021-07-23 11:37:32 +0900

    자바 if

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,25 +5,37 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
-		String str = s.nextLine();
+		int num = Integer.parseInt(s.nextLine());
 		s.close();
-		switch(str) {
-		case "a","A":
-			System.out.println("Apple");
-			break;
-		case "b","B":
-			System.out.println("Banana");
-			break;
-		case "w","W":
-			System.out.println("Watermelon");
-			break;
-		case "s","S":
-			System.out.println("Strwaberry");
-			break;
-		default:
-			System.out.println("모르는 이니셜");
-		}
-		System.out.println("프로그램 종료");
+		if(num>89)
+			System.out.println("A");
+		else if(num>79)
+			System.out.println("B");
+		else if(num>79)
+			System.out.println("C");
+		else if(num>79)
+			System.out.println("D");
+		else
+			System.out.println("F");
+	}
+}
+//		switch(str) {
+//		case "a","A":
+//			System.out.println("Apple");
+//			break;
+//		case "b","B":
+//			System.out.println("Banana");
+//			break;
+//		case "w","W":
+//			System.out.println("Watermelon");
+//			break;
+//		case "s","S":
+//			System.out.println("Strwaberry");
+//			break;
+//		default:
+//			System.out.println("모르는 이니셜");
+//		}
+//		System.out.println("프로그램 종료");
 //		if(str.equals("a") || str.equals("A"))
 //			System.out.println("Apple");
 //		else if(str.equals("b") || str.equals("B"))
@@ -35,5 +47,4 @@ public class Hello {
 //		else
 //			System.out.println("모르는 이니셜");
 //		System.out.println("프로그램 종료");
-	}
-}
+
