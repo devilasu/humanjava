0d38e5ca34b1ee0f05fc7260dcd9954462a03fcd
KYJ <devilasu@naver.com>
2021-07-27 17:38:50 +0900

    커밋

--- HelloWorld/src/Hello01.java
+++ HelloWorld/src/Hello01.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Hello {
+public class Hello01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ HelloWorld/src/Hello11.java
@@ -0,0 +1,9 @@
+
+public class Hello11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
