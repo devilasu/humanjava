8e564a00e80950ea2eedc82c2ada15f39d25a2f7
KYJ <devilasu@naver.com>
2021-07-29 09:40:50 +0900

    과제 추가

--- HelloWorld/src/Hello11.java
+++ HelloWorld/src/Hello11.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 
 public class Hello11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
 		
 	}
-
 }
---
+++ kimyoungjea/src/kimyoungjea/Computer.java
@@ -0,0 +1,8 @@
+package kimyoungjea;
+
+public class Computer {
+	int memory;			//메모리
+	int nCore;			//코어
+	int nPort;			//USB포트
+	String graphicCard;	//그래픽카드 모델
+}
